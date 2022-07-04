b106ac5dcb42a06a4f0bd2a5b2b6bf4eb1417d65
Danya <selyaninovdaniil@gmail.com>
2022-07-04 16:24:02 +0500

    + отслеживать мины,нажатие,обраб клика

--- saper.py
+++ saper.py
@@ -5,6 +5,30 @@ GRID_SIZE = 8 # Ширина и высота игрового поля
 SQUARE_SIZE = 50 # Размер одной клетки на поле
 MINES_NUM = 10 # Количество мин на поле
  
+mines = set(random.sample(range(1, GRID_SIZE**2+1), MINES_NUM))  # Генерируем мины в случайных позициях
+clicked = set()  # Создаем сет для клеточек, по которым мы кликнули
+ 
+# Функция реагирования на клик
+def click(event):
+    ids = c.find_withtag(CURRENT)[0]  # Определяем по какой клетке кликнули
+    if ids in mines:
+        c.itemconfig(CURRENT, fill="red") # Если кликнули по клетке с миной - красим ее в красный цвет
+    elif ids not in clicked:
+        c.itemconfig(CURRENT, fill="green") # Иначе красим в зеленый
+    c.update()
+ 
+# Функция для обозначения мин
+def mark_mine(event):
+    ids = c.find_withtag(CURRENT)[0]
+    # Если мы не кликали по клетке - красим ее в желтый цвет, иначе - в серый
+    if ids not in clicked:
+        clicked.add(ids)
+        x1, y1, x2, y2 = c.coords(ids)
+        c.itemconfig(CURRENT, fill="yellow")
+    else:
+        clicked.remove(ids)
+        c.itemconfig(CURRENT, fill="gray")
+
 root = Tk() # Основное окно программы
 root.title("Pythonicway Minesweep")
 c = Canvas(root, width=GRID_SIZE * SQUARE_SIZE, height=GRID_SIZE * SQUARE_SIZE) # Задаем область на которой будем рисовать
